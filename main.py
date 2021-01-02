6a6b06a1af1eb23e8ab77e09249ce774a34cfd6d
Jim Morrison <jim@sorced.com>
2021-01-01 20:04:58 -0800

    Add manual test metrics.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python3
+
 import os
 import single_read
 import monitoring
 
 DEVICE = os.environ.get('AIR_QUALITY_DEVICE', '/dev/ttyUSB0')
 LOCATION = os.environ.get(
-    'AIR_QUALITY_LOCATION', os.environ['HOSTNAME'])
+    'AIR_QUALITY_LOCATION', os.uname().nodename)
 
 
 pm25, pm10 = single_read.single_read(DEVICE)
--- monitoring.py
+++ monitoring.py
@@ -1,5 +1,8 @@
 import os
+import time
+
 from google.api import metric_pb2 as ga_metric
+from google.api import label_pb2
 from google.cloud import monitoring_v3
 
 
@@ -8,12 +11,11 @@ PROJECT = f'projects/{PROJECT_ID}'
 client = monitoring_v3.MetricServiceClient()
 
 
-def point(name, value):
+def point(location, name, value):
     series = monitoring_v3.TimeSeries()
-    series.metric.type = 'custom.googleapis.com/air_quality/particulates'
-    series.resource.type = '???'
-    series.resource.labels['location'] = location
-    series.resource.labels['size'] = name
+    series.metric.type = 'custom.googleapis.com/air_quality/particulates/' + name
+    series.resource.type = 'global'
+    series.metric.labels['location'] = location
     now = time.time()
     seconds = int(now)
     nanos = int((now - seconds) * 10 ** 9)
@@ -26,22 +28,36 @@ def point(name, value):
     return series
 
 
-def write_time_series(location, pm25, pm_10):
-    client.create_time_series(name=PROJECT, time_series=[point('2.5', pm25)])
-    client.create_time_series(name=PROJECT, time_series=[point('10', pm10)])
+def write_time_series(location, pm25, pm10):
+    client.create_time_series(
+        name=PROJECT, time_series=[point(location, '2.5', pm25)])
+    client.create_time_series(
+        name=PROJECT, time_series=[point(location, '10', pm10)])
 
 
-def create_metrics():
+def create_metric(name):
     descriptor = ga_metric.MetricDescriptor()
-    descriptor.type = 'custom.googleapis.com/air_quality/particulates'
+    descriptor.type = 'custom.googleapis.com/air_quality/particulates/' + name
     descriptor.metric_kind = ga_metric.MetricDescriptor.MetricKind.GAUGE
     descriptor.value_type = ga_metric.MetricDescriptor.ValueType.DOUBLE
     descriptor.description = 'Air quality particulates'
+    descriptor.labels.extend([
+      label_pb2.LabelDescriptor(
+          key='location',
+          value_type=label_pb2.LabelDescriptor.ValueType.STRING,
+          description='Location where the measurement was made.')
+    ])
     descriptor = client.create_metric_descriptor(
         name=PROJECT, metric_descriptor=descriptor
     )
     print('Created {}.'.format(descriptor.name))
 
 
+def create_metrics():
+  create_metric('2.5')
+  create_metric('10')
+
+
 if __name__ == '__main__':
   create_metrics()
+  write_time_series('test', 0.1, 0.2)
